45f60d55aa8d020c60ed9e63e11eda995597dd4b
chonamalus <chonamalus.bin@protonmail.com>
2023-09-07 09:34:11 +0200

    move the keyboard next to cursor

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 // Define your Sudoku puzzle as a 2D array (9x9)
-const sudokuPuzzle = [
+let sudokuPuzzle = [
   [5, 3, "", "", 7, "", "", "", ""],
   [6, "", "", 1, 9, 5, "", "", ""],
   ["", 9, 8, "", "", "", "", 6, ""],
@@ -10,7 +10,7 @@ const sudokuPuzzle = [
   ["", "", "", 4, 1, 9, "", "", 5],
   ["", "", "", "", 8, "", "", 7, 9]
 ];
-  
+
 // Function to generate the Sudoku grid
 function generateSudokuBoard() {
   const sudokuBoard = document.getElementById("sudoku-board");
@@ -21,28 +21,74 @@ function generateSudokuBoard() {
       const cell = document.createElement("div");
       cell.className = "cell";
       cell.textContent = sudokuPuzzle[row][col];
-      // Add a hover effect to change cursor type (FRONT)
-      cell.addEventListener("mouseenter", function () {
-        cell.style.cursor = "pointer";
+
+      // Add a hover effect to change cursor type for empty cells
+      if (sudokuPuzzle[row][col] === "") {
+        cell.addEventListener("mouseenter", function () {
+          cell.style.cursor = "pointer";
+        });
+        cell.addEventListener("mouseleave", function () {
+          cell.style.cursor = "default";
+        });
+
+      // Add click event to show the keyboard and pass the event object
+      cell.addEventListener("click", function (event) {
+        showKeyboard(row, col, event);
       });
-      cell.addEventListener("mouseleave", function () {
-        cell.style.cursor = "default";
-      });
-      sudokuBoard.appendChild(cell); // (BACK)
+        
+      }
+
+      sudokuBoard.appendChild(cell);
     }
   }
-
-  sudokuBoard.addEventListener('click', (e)=>{
-    if (e.target.className === 'cell') {
-      const currentValue = e.target.textContent;
-      const newValue = (Number(currentValue) % 9) + 1;
-      e.target.textContent = newValue;
-    }
-  });
 }
 
-// Call the function to generate the Sudoku board when the page is loaded (BACK)
+// Function to show the clickable keyboard next to the cursor
+function showKeyboard(row, col, event) {
+  const keyboard = document.getElementById("keyboard");
+  keyboard.innerHTML = ""; // Clear the keyboard
+
+  // Calculate the position of the cursor relative to the viewport
+  const cursorX = event.clientX;
+  const cursorY = event.clientY;
+
+  // Position the keyboard next to the cursor
+  keyboard.style.left = cursorX + "px";
+  keyboard.style.top = cursorY + "px";
+
+  // Generate the clickable keyboard buttons using a for loop
+  for (let i = 1; i <= 9; i++) {
+    const button = document.createElement("button");
+    button.className = "keyboard-button";
+    button.textContent = i;
+    button.onclick = function () {
+      inputNumber(i, row, col);
+    };
+    keyboard.appendChild(button);
+  }
+
+  // Add a button for digit 0
+  const button0 = document.createElement("button");
+  button0.className = "keyboard-button";
+  button0.textContent = "0";
+  button0.onclick = function () {
+    inputNumber(0, row, col);
+  };
+  keyboard.appendChild(button0);
+
+  keyboard.style.display = "block";
+}
+
+// Function to input a number into the Sudoku grid
+function inputNumber(number, row, col) {
+  sudokuPuzzle[row][col] = number;
+  const cell = document.querySelector(`.cell:nth-child(${row * 9 + col + 1})`);
+  cell.textContent = number;
+
+  // Hide the keyboard
+  const keyboard = document.getElementById("keyboard");
+  keyboard.style.display = "none";
+}
+
+// Call the function to generate the Sudoku board when the page is loaded
 document.addEventListener("DOMContentLoaded", generateSudokuBoard);
-// 'DOMContentLoaded' is an event in JavaScript that is fired when the HTML 
-// document has been completely loaded and parsed by the browser, and all the 
-// DOM elements are available for manipulation via JavaScript. 
