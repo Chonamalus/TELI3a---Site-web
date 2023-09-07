d1f8cd20d598f603e5dadae73bc5ba8363dac6e9
Francokab <francoiskabiry@yahoo.fr>
2023-09-07 09:23:52 +0200

    Added Timer

---
+++ timer.js
@@ -0,0 +1,35 @@
+ // Initialize variables
+ let seconds = 0;
+ let centiseconds = 0;
+
+ // Function to update the timer display
+ function updateTimerDisplay() {
+     const timerElement = document.getElementById("timer");
+     timerElement.textContent = seconds + "."+ centiseconds;
+ }
+
+function generateTimer() {
+    const timerElement = document.getElementById("timer");
+    timerElement.textContent = seconds + "." + centiseconds;
+    setInterval(IncrementTimer, 10);
+ }
+
+ function IncrementTimer() {
+    centiseconds++;
+    if (centiseconds == 100) {
+        seconds++;
+        centiseconds = 0 ;
+    }
+    updateTimerDisplay();
+ }
+
+
+ // Function to reset the timer
+ function resetTimer() {
+     seconds = 0;
+     centiseconds = 0;
+     updateTimerDisplay();
+ }
+
+ document.getElementById("resetButton").addEventListener("click", resetTimer);
+ document.addEventListener("DOMContentLoaded", generateTimer);
