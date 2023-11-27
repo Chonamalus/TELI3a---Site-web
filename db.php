08f429fa1c74a2a2ce7e79d87e972f4e9b9864e2
Francokab <francoiskabiry@yahoo.fr>
2023-11-27 12:39:58 +0100

    updated database connection identifiant

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect('10.35.58.205:3306', 'SiteWeb', 'azertyuiop','sudoku_schema');
+    $con = mysqli_connect('francois.kabiry.com:3306', 'siteweb', 'azertyuiop','siteweb');
     //Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
